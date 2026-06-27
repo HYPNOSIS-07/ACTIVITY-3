50acc0610874ab5c8832799c2038bc7cd3a5fa1e
HYPNOSIS-07 <vermareema0107@gmail.com>
2026-06-27 18:15:45 +0530

    Fibonacci Sequence

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,16 @@
+import sys
+n = int(input("Enter the number of terms: "))
+a, b = 0, 1
+print("Fibonacci Series:")
+for i in range(n):
+    print(a, end="  ")
+    a, b = b, a + b
+    print()
+    in range(n):
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    a, b = 0, 1
+    print("Fibonacci Series:")
+    for i in range(n):
+        print(a, end="  ")
+        a, b = b, a + b
